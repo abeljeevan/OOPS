3c7acd59ad555cc7b2698fdfb3674159fc987256
Abel Jeevan <168292754+abeljeevan@users.noreply.github.com>
2024-08-28 01:11:57 +0530

    Add files via upload

---
+++ sum of n.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+class Sumofdigits {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += s.nextInt();
+        }
+
+        s.close();
+        System.out.println(sum);
+    }
+}
